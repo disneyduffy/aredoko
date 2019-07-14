75894ba40b7ea4b0db101cedc45a6f6fbf3f96d4
TakuyaFujita <disneyduffy@gmail.com>
2019-07-14 09:15:33 +0900

    HTTPサービスの有効化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule }            from '@angular/core';
-import { BrowserModule }       from '@angular/platform-browser';
-import { FormsModule }         from '@angular/forms';
+import { NgModule }                       from '@angular/core';
+import { BrowserModule }                  from '@angular/platform-browser';
+import { FormsModule }                    from '@angular/forms';
+import { HttpClientModule }               from '@angular/common/http';
 
-import { AppComponent }        from './app.component';
-import { DashboardComponent }  from './dashboard/dashboard.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
-import { ItemsComponent }      from './items/items.component';
-import { MessagesComponent }   from './messages/messages.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }            from './in-memory-data.service';
 
-import { AppRoutingModule }    from './app-routing.module';
+import { AppComponent }                   from './app.component';
+import { DashboardComponent }             from './dashboard/dashboard.component';
+import { ItemDetailComponent }            from './item-detail/item-detail.component';
+import { ItemsComponent }                 from './items/items.component';
+import { MessagesComponent }              from './messages/messages.component';
+
+import { AppRoutingModule }               from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,11 @@ import { AppRoutingModule }    from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   bootstrap: [AppComponent]
 })
